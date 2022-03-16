a63603d3e8ed19b463e977edee745bf4b62e6234
Finch-H <merchantdaniel8@gmail.com>
2022-03-15 22:20:31 -1200

    Chat Room Removed +3others

--- lib/data.dart
+++ lib/data.dart
@@ -6,6 +6,10 @@
 // String myUrlAvatar = 'https://i.imgur.com/GXoYikT.png';
 
 // Napoleon
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:totalclinic/models/userProfile.dart';
+QuerySnapshot userProfileSnapshot;
+
 String myId = 'YB0XmxZ7KiZTHGtGRpue';
-String myUsername = 'Barack Obama';
+String myUsername = ;
 String myUrlAvatar = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/480px-President_Barack_Obama.jpg';
