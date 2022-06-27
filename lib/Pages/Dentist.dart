525dc43671405d4e6820deb5a35098876e3de9b8
Finch-H <merchantdaniel8@gmail.com>
2022-06-28 09:41:55 +1200

    dentist added

--- lib/Pages/Dentist.dart
+++ lib/Pages/Dentist.dart
@@ -20,118 +20,153 @@ class _DentistpageState extends State<Dentist> {
         title: Text("Dentists"),
       ),
       body: Column(
-children: [
- SingleChildScrollView(
-   scrollDirection: Axis.horizontal,
-   child: Row(
-      children: [
-
-
-          Row(
-          children: [
-             Padding(
-               padding: const EdgeInsets.all(20.0),
-               child: Container(
-                 height: 200,
-                 width: 200,
-                 child: Text(""),
-
-
-                   decoration: new BoxDecoration(
-                       image: new DecorationImage(
-                         image: new AssetImage("assets/images/DENTIST 2.jpg"),
-                         fit: BoxFit.fill,
-                       )
-                   )
-
-               ),
-             ),
-            
-            //061495
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                  height: 200,
-                  width:200,
-                  child: Text(" "),
-                  decoration: new BoxDecoration(
-                      image: new DecorationImage(
-                        image: new AssetImage("assets/images/DENTIST.jpg"),
-                        fit: BoxFit.fill,
-                      )
-                  )
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                  height: 200,
-                  width: 200,
-                  child: Text(" images "),
-
-                  decoration: new BoxDecoration(
-                      image: new DecorationImage(
-                        image: new AssetImage("assets/images/DENTIST3.jpg"),
-                        fit: BoxFit.fill,
-                      )
-                  )
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                  height: 200,
-                  width: 200,
-                  child: Text(" Images "),
-                  decoration: new BoxDecoration(
-                      image: new DecorationImage(
-                        image: new AssetImage("assets/images/DENTIST3.jpg"),
-                        fit: BoxFit.fill,
-                      )
-                  )
-              ),
-            )
-          ],
-        ),
-        // Show a dialog
-
-        // dentist department
-   
-      ],
-    ),
- ),
-
-
-  Padding(
-    padding: const EdgeInsets.only(top:10.0,left: 30.0, right: 30.0),
-    child: Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(25),
-      ),
-      color: Colors.blue,
-
-      // Book appointment.
-      child: Row(
         children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                          height: 200,
+                          width: 200,
+                          child: Text(""),
+                          decoration: new BoxDecoration(
+                              image: new DecorationImage(
+                            image:
+                                new AssetImage("assets/images/DENTIST 2.jpg"),
+                            fit: BoxFit.fill,
+                          ))),
+                    ),
+
+                    //061495
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                          height: 200,
+                          width: 200,
+                          child: Text(" "),
+                          decoration: new BoxDecoration(
+                              image: new DecorationImage(
+                            image: new AssetImage("assets/images/DENTIST.jpg"),
+                            fit: BoxFit.fill,
+                          ))),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                          height: 200,
+                          width: 200,
+                          child: Text(" images "),
+                          decoration: new BoxDecoration(
+                              image: new DecorationImage(
+                            image: new AssetImage("assets/images/DENTIST3.jpg"),
+                            fit: BoxFit.fill,
+                          ))),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                          height: 200,
+                          width: 200,
+                          child: Text(" Images "),
+                          decoration: new BoxDecoration(
+                              image: new DecorationImage(
+                            image: new AssetImage("assets/images/DENTIST3.jpg"),
+                            fit: BoxFit.fill,
+                          ))),
+                    )
+                  ],
+                ),
+                // Show a dialog
+
+                // dentist department
+              ],
+            ),
+          ),
           Row(
             children: [
-              Text(
-                " Book an Appointment ",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                  color: Colors.black26,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 130,
+                  height: 40,
+
+                  decoration: BoxDecoration(
+                      color: Color(0xFFB3CA2E6),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(70),
+                      )),
+
+                  // Book appointment.
+                  child: GestureDetector(
+                    onTap: (){
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text("BOOK AN APPOINTMENT",style: TextStyle(fontSize: 10, fontWeight: FontWeight.normal,color: Colors.black26),),
+                                ListTile(
+                                  leading: new Icon(Icons.photo),
+                                  title: new Text('Photo'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                ListTile(
+                                  leading: new Icon(Icons.music_note),
+                                  title: new Text('Music'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                ListTile(
+                                  leading: new Icon(Icons.videocam),
+                                  title: new Text('Video'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                ListTile(
+                                  leading: new Icon(Icons.share),
+                                  title: new Text('Share'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              ],
+                            );
+                          });
+                    },
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Text(
+                                " Book an Appointment ",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
         ],
-      ),
-    ),
-  ),
-],
-
-
       ),
     );
   }
--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -445,53 +445,113 @@ DatabaseReference _ref;
 
                     Container(
                       color: const Color(0xFFFFFFFF),
-                      height: 180,
+                      height: 250,
                       child: ListView(
-                        padding: EdgeInsets.zero,
+                        padding: EdgeInsets.all(10),
                         scrollDirection: Axis.horizontal,
                         children: <Widget>[
-                      SizedBox(
-                        height: 400,
-                        width: 200,
-                        child: GestureDetector(
+
+                         GestureDetector(
                           onTap: (){
                             Navigator.of(context).push(MaterialPageRoute(
                                 builder: (context) => Dentist()));
 
                           },
-                          child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: 150,
 
-                            child: Center(
-                                child: Text("Dentist")),
-                          color: Colors.white38,
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
 
-                            shadowColor: Colors.tealAccent,
+
+                              child: Center(
+                                  child: Text("Dentist",
+                                      style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),)),
+                              decoration: BoxDecoration(
+                                  color:Color(0xFFB3CA2E6),
+                                  borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(70),
+                                      topLeft: Radius.circular(10),
+                                  bottomLeft: Radius.circular(10),
+                                  bottomRight: Radius.circular(10)),
+
+
+                                  boxShadow: [
+                              BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 3), // changes position of shadow
+                            ),
+                            ],),
+
+                                                      ),
                           ),
                         ),
-                      ),
-                          SizedBox(
-                            height: 400,
-                            width: 200,
-                            child: Card(
 
-                              child: Center(
-                                  child: Text("Pharmacy")),
-                              color: Colors.white38,
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: 150,
+
+
+                                child: Center(
+                                    child: Text("Pharmacy",
+                                      style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),)),
+
+                                decoration: BoxDecoration(
+                                    color: Color(0xFFB3CA2E6),
+                                    borderRadius: BorderRadius.only(topRight: Radius.circular(70),
+                                        topLeft: Radius.circular(10),
+                                        bottomLeft: Radius.circular(10),
+                                        bottomRight: Radius.circular(10)),
+                                    boxShadow: [
+                                BoxShadow(
+                                color: Colors.black26,
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: Offset(0, 3), // changes position of shadow
+                              ),
+                              ],
                             ),
+
+
+
+                                ),
                           ),
-                          SizedBox(
-                            height: 400,
-                            width: 200,
-                            child: Card(
 
-                              child: Center(
-                                  child: Text("Paediatrician")),
-                              color: Colors.white38,
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                            ),
-                          )
+
+                       Padding(
+                         padding: const EdgeInsets.all(8.0),
+                         child: Container(
+                           width:150,
+
+                                child: Center(
+                                    child: Text("Paediatrician",
+                                      style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),)),
+                               // color: Colors.white38,
+                                decoration: BoxDecoration(
+                                    color:  Color(0xFFB3CA2E6),
+                                    borderRadius: BorderRadius.only(topRight: Radius.circular(70),
+                                    topLeft: Radius.circular(10),
+                                    bottomLeft: Radius.circular(10),
+                                    bottomRight: Radius.circular(10)),
+
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 5,
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3), // changes position of shadow
+                                    ),
+                                  ],
+                                ),
+
+
+                                ),
+                       ),
+
+
                         ],
                       ),
                     ),
