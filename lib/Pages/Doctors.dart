73cb60433ab5463fd9500e40b3cb4ed1328c838f
MR.NARTERH <merchantdaniel8@gmail.com>
2023-03-12 21:22:47 +0000

    doctorspage

---
+++ lib/Pages/Doctors.dart
@@ -0,0 +1,16 @@
+class Doctors extends StatefulWidget {
+  const Doctors({Key? key}) : super(key: key);
+
+  @override
+  State<Doctors> createState() => _DoctorsState();
+}
+
+class _DoctorsState extends State<Doctors> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+      body: Container()
+    );
+  }
+}
